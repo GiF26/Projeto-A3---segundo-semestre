57f04853cdcd1d59625e4b15f44ce647b3befac6
Giovanna Ferreira <83253307+GiF26@users.noreply.github.com>
2025-05-27 14:25:11 -0300

    ********* Novo repositorio *********
    - Nova organização de telas

---
+++ A3/src/main/java/com/mycompany/a3/A3.java
@@ -0,0 +1,11 @@
+package com.mycompany.a3;
+
+import com.mycompany.a3.TelaLogin.TelaLogin;
+
+public class A3 {
+
+    public static void main(String[] args) {
+        TelaLogin main =  new TelaLogin();
+        main.setVisible(true);
+    }
+}
